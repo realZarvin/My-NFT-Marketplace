7b834b618fc536a6652e244f3bd77c4ef0aa4585
Wali Chizarum <155149253+realZarvin@users.noreply.github.com>
2024-08-03 00:03:24 +0100

    Update deploy-contract.js

--- Truffle-Migration/deploy-contract.js
+++ Truffle-Migration/deploy-contract.js
@@ -1,1 +1,11 @@
+const NFT = artifacts.require("NFT");
+const Marketplace = artifacts.require("Marketplace");
+const Royalties = artifacts.require("Royalties");
+
+module.exports = function (deployer) {
+  deployer.deploy(NFT).then(function () {
+    return deployer.deploy(Marketplace, NFT.address);
+  });
+  deployer.deploy(Royalties);
+};
 

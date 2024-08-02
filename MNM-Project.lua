ab0da55e6266f76ac7c553583724c5ef6e295a34
Wali Chizarum <155149253+realZarvin@users.noreply.github.com>
2024-08-02 23:48:47 +0100

    Create MNM-Project.lua

---
+++ MNM-Project.lua
@@ -0,0 +1,17 @@
+Zarvin first try of a basic nft marketplace/
+├── contracts/
+│   ├── NFT.sol
+│   ├── Marketplace.sol
+│   └── Royalties.sol
+├── migrations/
+│   └── 2_deploy_contracts.js
+├── test/
+│   ├── Marketplace.test.js
+│   ├── NFT.test.js
+│   └── Royalties.test.js
+├── scripts/
+│   └── deploy.js
+├── truffle-config.js
+├── hardhat.config.js
+├── package.json
+└── README.md

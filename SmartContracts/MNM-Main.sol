12a3d4eeda1c0e38dd56f705a6338ea1dfb91336
Wali Chizarum <155149253+realZarvin@users.noreply.github.com>
2024-08-02 23:53:32 +0100

    Update MNM-Main.sol

--- SmartContracts/MNM-Main.sol
+++ SmartContracts/MNM-Main.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract NFT is ERC721, Ownable {
+    uint256 public tokenCounter;
+
+    constructor() ERC721("NFT", "NFT") {
+        tokenCounter = 0;
+    }
+
+    function createNFT(string memory tokenURI) public onlyOwner returns (uint256) {
+        uint256 newItemId = tokenCounter;
+        _safeMint(msg.sender, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+        tokenCounter++;
+        return newItemId;
+    }
+}
 
